e9082529ae01dfc06274cade12435395fcc3f58c
Miranti20 <rantirarrr4505@gmail.com>
2025-05-23 09:23:40 +0700

    membuat case 1

--- stacKusingLinkedList_0278.cpp
+++ stacKusingLinkedList_0278.cpp
@@ -29,10 +29,10 @@ public:
     // Push operation Insert an element onto the top of the stack
     int push(int value) // operasi push
     {
-        Node *newNode = new Node(); //1. Allocate memory for the new node
-        newNode->data = value; //2. assign value 
-        newNode->next = top; // 3. set the next pointer of the new node 
-        top = newNode; // 4. update the top pointer into the new node
+        Node *newNode = new Node(); // 1. Allocate memory for the new node
+        newNode->data = value;      // 2. assign value
+        newNode->next = top;        // 3. set the next pointer of the new node
+        top = newNode;              // 4. update the top pointer into the new node
         cout << "Push value: " << value << endl;
         return value;
     }
@@ -45,33 +45,33 @@ public:
         }
 
         Node *temp = top; // create temporary pointer
-        top = top->next; // update the top pointer to the next node
+        top = top->next;  // update the top pointer to the next node
         cout << "Popped value: " << temp->data << endl;
         delete temp;
     }
 
-void peek()
-{
-    if (top == NULL)
+    void peek()
     {
-        cout << "List is empty." << endl;
-    }
-    else 
-    {
-        Node *current = top;
-        while (current != NULL)
+        if (top == NULL)
         {
-            cout << current->data << " " << endl;
-            current = current->next;
+            cout << "List is empty." << endl;
         }
-        cout << endl;
-    } // retrunt the value of the top node
-}
-// is empty operation, check is the stack empty
-bool isEmpty()
-{
-    return top == NULL; // return the if too pointer us null.indicate an empty stack
-}
+        else
+        {
+            Node *current = top;
+            while (current != NULL)
+            {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        } // retrunt the value of the top node
+    }
+    // is empty operation, check is the stack empty
+    bool isEmpty()
+    {
+        return top == NULL; // return the if too pointer us null.indicate an empty stack
+    }
 };
 
 int main()
@@ -82,12 +82,20 @@ int main()
     int value;
 
     while (choice != 4)
-    
+    {
+        cout << "1. Push\n";
+        cout << "2. Pop\n";
+        cout << "3. Peek\n";
+        cout << "4. Exit\n";
+        cin >> choice;
 
+        switch (choice)
+        {
+        case 1:
+            cout << "Enter the valuse to push: ";
+            cin >> value;
+            stack.push(value); // push the entered value onto the stack
+            break;
+        }
+    }
 }
-
-
-
-
-
-
