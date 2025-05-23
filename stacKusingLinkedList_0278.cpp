cedc2e9b4c60b911f682921430d883f5787c8024
Miranti20 <rantirarrr4505@gmail.com>
2025-05-23 08:02:36 +0700

    Menambahkan to the top node of the stack

--- stacKusingLinkedList_0278.cpp
+++ stacKusingLinkedList_0278.cpp
@@ -14,3 +14,18 @@ public:
         next = NULL;
     }
 };
+
+//Stack class 
+class stack 
+{
+    Node *top; // Pointer to the top node of the stack
+
+public:
+    stack ()
+    {
+        top = NULL; // Intialize the stack with a null top pointer
+    }
+    // Push operation Insert an element onto the top of the stack
+    int push(int value)
+    {
+}
