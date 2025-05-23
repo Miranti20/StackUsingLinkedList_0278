cd6e73014228b0af012cdb5f7cfdce6c751bcc21
Miranti20 <rantirarrr4505@gmail.com>
2025-05-23 08:33:56 +0700

    Mulai implementasi operasi pop dengan pengecekan stack kosong

--- stacKusingLinkedList_0278.cpp
+++ stacKusingLinkedList_0278.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-//Node class representing a single node in the linked list
+// Node class representing a single node in the linked list
 class Node
 {
 public:
@@ -15,16 +15,17 @@ public:
     }
 };
 
-//Stack class 
-class stack 
+// Stack class
+class stack
 {
     Node *top; // Pointer to the top node of the stack
 
 public:
-    stack ()
+    stack()
     {
         top = NULL; // Intialize the stack with a null top pointer
     }
+
     // Push operation Insert an element onto the top of the stack
     int push(int value) // operasi push
     {
@@ -35,6 +36,17 @@ public:
         cout << "Push value: " << value << endl;
         return value;
     }
-}
 
+    void pop() // pop operation: remove top most element from the stack
+    {
+        if (isEmpty())
+        {
+            cout << "Stuck is empty." << endl;
+        }
+
+        Node *temp = top; // create temporary pointer
+        top = top->next; // update the top pointer to the next node
+        cout << "Popped value: " << top->data << endl;
+    }
+}
 
